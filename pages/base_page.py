dd50f5f691cecce6c098fff5b5808c639f259a03
Jane-Hopp <natyanarniya@gmail.com>
2022-08-26 13:00:46 +0300

    Catching an exception with assert. Command for implicit wait.

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,5 @@
+from selenium.common.exceptions import NoSuchElementException 
+
 class BasePage():
     def __init__(self, browser, url):
         self.browser = browser
@@ -5,3 +7,17 @@ class BasePage():
 
     def open(self): 
         self.browser.get(self.url)
+
+    #команда для неявного ожидания со значением по умолчанию в 10
+    def __init__(self, browser, url, timeout=10):
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(timeout)
+
+    #метод для перехвата исключений
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
